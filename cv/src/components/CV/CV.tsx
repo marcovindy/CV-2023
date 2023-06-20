c8f52ea6551a556ad94ea9bce10fc4555ba89fbf
Marek Vaníček <marcovindy@gmail.com>
2023-06-20 15:36:08 +0200

    feat(Header and scrolling with hooks)

--- cv/src/components/CV/CV.tsx
+++ cv/src/components/CV/CV.tsx
@@ -1,28 +1,63 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useFela } from 'react-fela';
 
-import {Header} from '../Header';
-import {PersonalInfo} from '../PersonalInfo';
-import {Education} from '../Education';
-import {WorkExperience} from '../WorkExperience';
-import {Skills} from '../Skills';
-import {Projects} from '../Projects';
-import {Contact} from '../Contact';
+import { Header } from '../Header';
+import { PersonalInfo } from '../PersonalInfo';
+import { Education } from '../Education';
+import { WorkExperience } from '../WorkExperience';
+import { Skills } from '../Skills';
+import { Projects } from '../Projects';
+import { Contact } from '../Contact';
 
 export interface CVProps {}
 
 export const CV = ({}: CVProps) => {
   const { css } = useFela();
 
+  // Reference na jednotlivé sekce pro plynulé scrollování
+  const personalInfoRef = useRef<HTMLDivElement>(null);
+  const educationRef = useRef<HTMLDivElement>(null);
+  const workExperienceRef = useRef<HTMLDivElement>(null);
+  const skillsRef = useRef<HTMLDivElement>(null);
+  const projectsRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
+
+  const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
+    if (ref.current) {
+      ref.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <div className={css(styles.container)}>
-      <Header/>
-      <PersonalInfo />
-      <Education />
-      <WorkExperience />
-      <Skills />
-      <Projects />
-      <Contact />
+    <div>
+      <Header
+        onPersonalInfoClick={() => scrollToSection(personalInfoRef)}
+        onEducationClick={() => scrollToSection(educationRef)}
+        onWorkExperienceClick={() => scrollToSection(workExperienceRef)}
+        onSkillsClick={() => scrollToSection(skillsRef)}
+        onProjectsClick={() => scrollToSection(projectsRef)}
+        onContactClick={() => scrollToSection(contactRef)}
+      />
+      <div className={css(styles.container)}>
+        <div ref={personalInfoRef}>
+          <PersonalInfo />
+        </div>
+        <div ref={educationRef}>
+          <Education />
+        </div>
+        <div ref={workExperienceRef}>
+          <WorkExperience />
+        </div>
+        <div ref={skillsRef}>
+          <Skills />
+        </div>
+        <div ref={projectsRef}>
+          <Projects />
+        </div>
+        <div ref={contactRef}>
+          <Contact />
+        </div>
+      </div>
     </div>
   );
 };
--- cv/src/components/Header/Header.tsx
+++ cv/src/components/Header/Header.tsx
@@ -1,11 +1,58 @@
- 
+import React from "react";
 
-export interface HeaderProps {}
+export interface HeaderProps {
+  onPersonalInfoClick: () => void;
+  onEducationClick: () => void;
+  onWorkExperienceClick: () => void;
+  onSkillsClick: () => void;
+  onProjectsClick: () => void;
+  onContactClick: () => void;
+}
 
-export const Header = ({}: HeaderProps) => {
+export const Header = ({
+  onPersonalInfoClick,
+  onEducationClick,
+  onWorkExperienceClick,
+  onSkillsClick,
+  onProjectsClick,
+  onContactClick,
+}: HeaderProps) => {
   return (
-    <>
-    
-    </>
-  );  
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#personalInfo" onClick={onPersonalInfoClick}>
+              Personal Info
+            </a>
+          </li>
+          <li>
+            <a href="#education" onClick={onEducationClick}>
+              Education
+            </a>
+          </li>
+          <li>
+            <a href="#workExperience" onClick={onWorkExperienceClick}>
+              Work Experience
+            </a>
+          </li>
+          <li>
+            <a href="#skills" onClick={onSkillsClick}>
+              Skills
+            </a>
+          </li>
+          <li>
+            <a href="#projects" onClick={onProjectsClick}>
+              Projects
+            </a>
+          </li>
+          <li>
+            <a href="#contact" onClick={onContactClick}>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
